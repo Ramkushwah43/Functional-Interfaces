00c89807fdad1c3d8730b5ed28813be8b455b853
Ramu Kushwah <kushwahramu2308@gmail.com>
2025-10-24 19:59:48 +0530

    add one or interface Internal Working of LongSupplier Functional Interface...

---
+++ LongSupplierDemo.java
@@ -0,0 +1,9 @@
+// LongSupplier interface
+/**
+	Internal Working of LongSupplier Functional Interface...
+**/
+public interface LongSupplierDemo
+{
+	//Supplier functional interface contain only one abstract method**
+	public abstract long getAsLong();
+}
